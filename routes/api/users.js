15515eac8043ab5089792a2da4938042a08b3c32
marb61a <marb61a@gmail.com>
2018-05-16 16:56:17 +0100

    added register route to users.js and installed gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravater');
+
+// Import User model
+const User = require('../../models/User');
 
 // @route GET /routes/api/users/test
 // @desc Tests users route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "Users file works"}));
+
+// @route POST /routes/api/users/register
+// @desc Register user
+// @access Public
+router.post('/register', (req, res) => {
+  // Check to see if email exists when registering
+  User.findOne({email: req.body.email})
+    .then(user => {
+      if(user){
+        return res.status(400).json({email: 'Email already exists'});
+      } else {
+        const avatar = gravatar.url();
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+      }
+    });
+});
 
 module.exports = router;
