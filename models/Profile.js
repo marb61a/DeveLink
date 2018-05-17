9fd958279bf7ce0154abef5d8753307f04639e7b
marb61a <marb61a@gmail.com>
2018-05-17 16:49:27 +0100

    updated profile model with experience and education sections

--- models/Profile.js
+++ models/Profile.js
@@ -34,5 +34,64 @@ const ProfileSchema = new Schema({
   },
   githubusername: {
     type: String
-  }
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      }
+    }
+  ]
 });
