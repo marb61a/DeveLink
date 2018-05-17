d7c59868a01d6d9966de09985d0bc4616c6b3d1c
marb61a <marb61a@gmail.com>
2018-05-17 12:06:48 +0100

    added initial validation for registering and updated current user token validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -104,8 +104,12 @@ router.post('/login', (req, res) => {
 // @route POST /routes/api/users/current
 // @desc Return current user
 // @access Private
-router.get('/current', passport.authenticate(jwt, {session: false}), () => {
-  
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
---
+++ validation/register.js
@@ -0,0 +1,9 @@
+const Validator = require('validator');
+
+module.exports = function validateRegisterInput(data){
+  let errors = {};
+
+  if(!Validator.isLength(data.name, {min: 2, max: 30})){
+
+  }
+};
