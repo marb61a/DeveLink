a7ea55995adf4cb1162dc4b6dcf9e89be1a57477
marb61a <marb61a@gmail.com>
2018-05-17 17:28:27 +0100

    added get and post route to profile api routes file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,6 +18,31 @@ router.get('/test', (req, res) => res.json({msg: "Profile file works"}));
 // @desc Gets the current users profile 
 // @access Private
 router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const errors = {};
+
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      if(!profile){
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route POST /routes/api/profile
+// @desc Create user profile 
+// @access Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  // Get the profile fields
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if(req.body.handle) profileFields.handle = req.body.handle;
+  if(req.body.company) profileFields.company = req.body.company;
+  if(req.body.website) profileFields.website = req.body.website;
+  if(req.body.location) profileFields.location = req.body.location;
 
 });
 
