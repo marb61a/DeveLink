ba657b32526dd91cc9dd75d05543c61e06c46bc8
marb61a <marb61a@gmail.com>
2018-05-18 17:33:54 +0100

    added getting user experience route to profile routes and initial experience validation file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -53,7 +53,7 @@ router.get('all', (req, res) => {
     res.json(profiles);
   })
   .catch(err => res.status(404).json(err));
-  
+
 });
 
 // @route GET /api/profile/handle/:handle
@@ -156,6 +156,30 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
               .then(profile => res.json(profile));
           });
       }
+    });
+});
+
+// @route POST /api/profile/experience
+// @desc Add experience to a user profile
+// @access Private
+router.post('/experience', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+
+      // Add to experience array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile));
+      
     });
 });
 
---
+++ validation/experience.js
@@ -0,0 +1,7 @@
+const Validator = require('validator');
+const isEmpty = require ('./is-empty');
+
+module.exports = function validateExperienceInput(data){
+  let errors = {};
+
+};
