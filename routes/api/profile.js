b8bb3cd9a576b864aa0158bf193f5f873dc4d146
marb61a <marb61a@gmail.com>
2018-05-20 12:03:20 +0100

    added delete education route to profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -225,4 +225,29 @@ router.post('/education', passport.authenticate('jwt', {session: false}), (req, 
     });
 });
 
+// @route DELETE /api/profile/experience/:exp_id
+// @desc Delete experince from a user profile
+// @access Private
+router.delete('/experience/:exp_id', passport.authenticate('jwt', {session: false}), 
+(req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      // Get remove index
+      const removeIndex = profile.experience
+        .map(item => item.id)
+        .indexOf(req.params.exp_id);
+      
+      // Splice out of the array
+      profile.experience.splice(removeIndex, 1);
+
+      // Save the profile
+      profile.save(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route DELETE /api/profile/experience/:exp_id
+// @desc Delete education from a user profile
+// @access Private
+
 module.exports = router;
