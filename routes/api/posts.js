e6d5b266d1a35be917eb777a872d5b275d1b71da
marb61a <marb61a@gmail.com>
2018-05-21 17:34:56 +0100

    added validations to posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require('passport');
 // Import post model
 const Post = require('../../models/Post');
 
+// Import validations
+const validatePostInput = require('../../validation/post');
+
 // @route GET /routes/api/posts/test
 // @desc Tests posts route
 // @access Public
@@ -15,6 +18,14 @@ router.get('/test', (req, res) => res.json({msg: "Posts file works"}));
 // @desc Create post
 // @access Private 
 router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const {errors, isValid} = validatePostInput(req.body);
+
+  // Check validation
+  if(!isValid){
+    // If there are any errors, send a 400 with errors objects
+    return res.status(400).json(errors);
+  }
+
   const newPost = new Post({
     text: req.body.text,
     name: req.body.name,
