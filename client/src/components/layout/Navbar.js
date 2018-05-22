76dcc866311898b37e9e23b284cbc8438c51cccb
marb61a <marb61a@gmail.com>
2018-05-22 17:04:21 +0100

    added footer component and initial landing component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,25 +3,25 @@ import React, { Component } from 'react';
 class Navbar extends Component {
   render(){
     return(
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div class="container">
-          <a class="navbar-brand" href="landing.html">DeveLink</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <div className="container">
+          <a className="navbar-brand" href="landing.html">DeveLink</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="mobile-nav">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="profiles.html"> Developers
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="profiles.html"> Developers
                 </a>
               </li>
             </ul>
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="register.html">Sign Up</a>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="register.html">Sign Up</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="login.html">Login</a>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">Login</a>
               </li>
             </ul>
           </div>
