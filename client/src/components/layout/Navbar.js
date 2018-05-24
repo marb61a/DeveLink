0a3bd47ff04ba4a03e17bd2d4e6730b432e48de2
marb61a <marb61a@gmail.com>
2018-05-24 18:06:55 +0100

    updated navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,40 @@ import { connect } from 'react-redux';
 import {logoutUser} from '../../actions/authActions';
 
 class Navbar extends Component {
+  onLogoutClick(e){
+    e.preventDefault();
+  } 
+
   render(){
+    const { isAuthenticated, user } = this.props.auth;
+
+    const authLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a href="" 
+            onClick={this.onLogoutClick.bind(this)}
+            className="nav-link"
+          >
+          </a>
+          <img 
+          
+          />
+          Logout
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/register">Sign Up</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">Login</Link>
+        </li>
+      </ul>
+    )
+
     return(
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
         <div className="container">
@@ -17,18 +50,14 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/profiles"> 
+                <Link className="nav-link" to="/profiles">
+                  {' '}
                   Developers
                 </Link>
               </li>
             </ul>
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">Sign Up</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
+              
             </ul>
           </div>
         </div>
