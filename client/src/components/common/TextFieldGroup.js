8e5e3d229aca4284eb932c02230930301cb7a744
marb61a <marb61a@gmail.com>
2018-05-24 21:13:08 +0100

    added TextFieldGroup components to auth files to replace existing div sections

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -32,4 +32,19 @@ const TextFieldGroup = ({
   );
 }
 
+TextFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.string
+}
+
+TextFieldGroup.defaultProps = {
+  type: 'text'
+}
+
 export default TextFieldGroup;
