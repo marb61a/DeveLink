6a777eaa229cef79d88a1d935fef169a86b1ce99
marb61a <marb61a@gmail.com>
2018-05-27 12:03:57 +0100

    added TextAreaFieldGroup initial file and updated CreateProfile

---
+++ client/src/components/common/TextAreaFieldGroup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const TextAreaFieldGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  info,
+  onChange
+}) => {
+  return (
+    <div className="form-group">
+    
+    </div>
+  );
+};
+
+export default TextAreaFieldGroup;
--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import TextFieldGroup from '../common/TextFieldGroup';
 
 class CreateProfile extends Component {
   constructor(props){
@@ -26,14 +27,26 @@ class CreateProfile extends Component {
 
   render() {
     return (
-      <div>
-        
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Enter Your Info To Make Your Profile Stand Out
+              </p>
+              <small className="d-block pb-3">
+                * = required fields
+              </small>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-PrivateRoute.propTypes = {
+CreateProfile.propTypes = {
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired
 };
