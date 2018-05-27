5f3d212b7927b8285e365e8f1084ce7ef30ce227
marb61a <marb61a@gmail.com>
2018-05-27 11:41:34 +0100

    added create proile file and folder

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class CreateProfile extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      handle: '',
+      company: '',
+      website: '',
+      location: '',
+      status: '',
+      skills: '',
+      githubnusername: '',
+      bio: '',
+      twitter: '',
+      facebook: '',
+      linkedin: '',
+      youtube: '',
+      instagram: '',
+      errors: {}
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+PrivateRoute.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect (mapStateToProps)(CreateProfile);
