7442c88abaf86c92d6f9b2dc6dbe510e343e79dd
marb61a <marb61a@gmail.com>
2018-05-27 18:09:30 +0100

    added InputGroup initial file and updated SelectListGroup file

---
+++ client/src/components/common/InputGroup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+const InputGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  icon,
+  type,
+  onChange
+}) => {
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+InputGroup.propTypes = {
+
+};
+
+InputGroup.defaultProps = {
+  type: 'text'
+};
+
+export default InputGroup;
--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -2,10 +2,27 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const SelectListGroup = () => {
+const SelectListGroup = ({name, value, error, info, onChange, options}) => {
+  const selectOptions = options.map(
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </option>
+  );
+
   return (
     <div className="form-group">
-    
+      <select
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error
+        })}
+        name={name}
+        value={value}
+        onChange={onChange}
+      >
+        {selectOptions}
+      </select>
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
