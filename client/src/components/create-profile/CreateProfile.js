cc98359d2bd1a5f83055a15bd067ccad4bf42539
marb61a <marb61a@gmail.com>
2018-05-28 11:34:13 +0100

    added TextFieldGroup sections for status and company to CreateProfile

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -37,12 +37,25 @@ class CreateProfile extends Component {
 
   }
 
-  onChange(){
+  onChange(e){
     this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
     const { errors } = this.state;
+
+    // Select options for status
+    const options = [
+      { label: '* Select Professional Status', value: 0 },
+      { label: 'Developer', value: 'Developer' },
+      { label: 'Junior Developer', value: 'Junior Developer' },
+      { label: 'Senior Developer', value: 'Senior Developer' },
+      { label: 'Manager', value: 'Manager' },
+      { label: 'Student or Learning', value: 'Student or Learning' },
+      { label: 'Instructor or Teacher', value: 'Instructor or Teacher' },
+      { label: 'Intern', value: 'Intern' },
+      { label: 'Other', value: 'Other' }
+    ]
 
     return (
       <div className="create-profile">
@@ -65,6 +78,23 @@ class CreateProfile extends Component {
                   error={errors.handle}
                   info="A unique handle for your profile URL. Your full name, 
                   company name, nickname"
+                />
+                <SelectListGroup 
+                  placeholder="Status"
+                  name="status"
+                  value={this.state.status}
+                  onChange={this.onChange}
+                  options={options}
+                  error={errors.status}
+                  info="Where are you at in your career?"
+                />
+                <TextFieldGroup 
+                  placeholder="Company"
+                  name="company"
+                  value={this.state.company}
+                  onChange={this.onChange}
+                  error={errors.company}
+                  info="Could be your own company or one you work for"
                 />
               </form>
             </div>
