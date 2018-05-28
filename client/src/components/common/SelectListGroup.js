2721e0093a7405558a27e70261e1282b52484991
marb61a <marb61a@gmail.com>
2018-05-28 11:24:34 +0100

    added TextFieldGroup section and onChange and onSubmit methods to CreateProfile

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -3,11 +3,11 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 const SelectListGroup = ({name, value, error, info, onChange, options}) => {
-  const selectOptions = options.map(
+  const selectOptions = options.map( option => (
     <option key={option.label} value={option.value}>
       {option.label}
     </option>
-  );
+  ));
 
   return (
     <div className="form-group">
--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -27,9 +27,23 @@ class CreateProfile extends Component {
       instagram: '',
       errors: {}
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+
+  }
+
+  onChange(){
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="create-profile">
         <div className="container">
@@ -42,6 +56,17 @@ class CreateProfile extends Component {
               <small className="d-block pb-3">
                 * = required fields
               </small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup 
+                  placeholder="* Profile Handle"
+                  name="Handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL. Your full name, 
+                  company name, nickname"
+                />
+              </form>
             </div>
           </div>
         </div>
