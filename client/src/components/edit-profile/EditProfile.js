ba51f94683f1808424959c7c3aacfdec45abc379
marb61a <marb61a@gmail.com>
2018-05-28 21:17:54 +0100

    updated EditProfile render and return

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
@@ -44,7 +45,19 @@ class CreateProfile extends Component{
     }
 
     if(nextProps.profile.profile){
+      const profile = nextProps.profile.profile;
 
+      // Bring the skills array back to csv
+      const skillsCSV = profile.skills.join(',');
+
+      // If a profile field does not exist then make an empty string
+      profile.company = !isEmpty(profile.company) ? profile.company : '';
+      profile.website = !isEmpty(profile.website) ? profile.website : '';
+      profile.location = !isEmpty(profile.location) ? profile.location : '';
+      profile.githubusername = !isEmpty(profile.githubusername) ? profile.githubusername : '';
+      profile.bio = !isEmpty(profile.bio) ? profile.bio : '';
+      profile.social = !isEmpty(profile.social) ? profile.social : {};
+      
     }
   }
 
@@ -68,6 +81,71 @@ class CreateProfile extends Component{
     };
 
     this.props.createProfile(profileData, this.props.history)
+  }
+
+  onChange(e){
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render(){
+    const { errors, displaySocialInputs } = this.state;
+
+    let socialInputs;
+
+    if(displaySocialInputs){
+      socialInputs = (
+        <div>
+          <InputGroup
+            placeholder="Twitter Profile URL"
+            name="twitter"
+            icon="fab fa-twitter"
+            value={this.state.twitter}
+            onChange={this.onChange}
+            error={errors.twitter}
+          />
+          <InputGroup
+            placeholder="Facebook Page URL"
+            name="facebook"
+            icon="fab fa-facebook"
+            value={this.state.facebook}
+            onChange={this.onChange}
+            error={errors.facebook}
+          />
+          <InputGroup
+            placeholder="Linkedin Profile URL"
+            name="linkedin"
+            icon="fab fa-linkedin"
+            value={this.state.linkedin}
+            onChange={this.onChange}
+            error={errors.linkedin}
+          />
+          <InputGroup
+            placeholder="YouTube Channel URL"
+            name="youtube"
+            icon="fab fa-youtube"
+            value={this.state.youtube}
+            onChange={this.onChange}
+            error={errors.youtube}
+          />
+          <InputGroup
+            placeholder="Instagram Page URL"
+            name="instagram"
+            icon="fab fa-instagram"
+            value={this.state.instagram}
+            onChange={this.onChange}
+            error={errors.instagram}
+          />
+        </div>
+      );
+    }
+
+    return(
+      <div className="create-profile">
+        <div className="container">
+        
+        </div>
+      </div>
+    );
   }
 }
 
