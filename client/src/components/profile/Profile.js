f368704486b8dea24e9d318d435272afbb1bcea1
marb61a <marb61a@gmail.com>
2018-05-30 21:03:49 +0100

    updated Profile.js

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,15 +4,50 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import Spinner from '../common/Spinner';
+import ProfileHeader from './ProfileHeader';
+import ProfileAbout from './ProfileAbout';
+import ProfileCreds from './ProfileCreds';
+import ProfileGithub from './ProfileGithub';
+import { getProfileByHandle } from '../../actions/profileActions';
 
 class Profile extends Component {
+  componentDidMount(){
+    if(this.props.match.params.handle){
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
+
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if(profile === null || loading){
+      profileContent = <Spinner />;
+    } else {
+
+    }
+
     return (
-      <div>
-        
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              Profile Content
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default connect()(Profile);
+Profile.propTypes = {
+  getProfileByHandle: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, {getProfileByHandle})(Profile);
