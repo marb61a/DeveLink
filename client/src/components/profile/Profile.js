370758532ed2e3a2936688d2a09622ee2c4b4cf5
marb61a <marb61a@gmail.com>
2018-05-30 17:32:18 +0100

    added initial profile folder and files

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import Spinner from '../common/Spinner';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default connect()(Profile);
