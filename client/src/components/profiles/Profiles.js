4f5f9f8b7cddf704c780406a184329ec351fb29a
marb61a <marb61a@gmail.com>
2018-05-30 13:14:06 +0100

    updated Profiles file

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
 import Spinner from '../common/Spinner';
+import ProfileItem from './ProfileItem';
+import { getProfiles } from '../../actions/profileActions';
 
 class Profiles extends Component {
+  componentDidMount(){
+    this.props.getProfiles();
+  }
+
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileItems;
+
+    if(profiles === null || loading){
+      profileItems = <Spinner />;
+    } else {
+      if(profiles.length > 0 ){
+        <h1>Profiles Here</h1>
+      } else {
+        profileItems = <h4>No Profiles Found</h4>
+      }
+    }
+
     return (
-      <div>
-        
+      <div className="profiles">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+            
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Profiles;
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+
+export default connect(mapStateToProps, {getProfiles})(Profiles);
