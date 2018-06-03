0ae128e5cd0542b0f931a99da536c318cdd6ca61
marb61a <marb61a@gmail.com>
2018-06-03 19:08:46 +0100

    updated key file

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  process.env.MONGO_URI: 'mongodb://martin:MyMLabMay2018@ds119160.mlab.com:19160/develink',
-  secretOrKey: process.env.SECRET_OR_KEY 
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 };
