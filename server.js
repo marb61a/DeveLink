fc94bb2a21b32535892f9c23c9b5549dcd367b2d
marb61a <marb61a@gmail.com>
2018-06-03 18:56:27 +0100

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const path = require("path");
+const apath = require("path");
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(apath.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
